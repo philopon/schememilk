ca6dcec0f8e210a5541ec594f620d9c7bb7f8764
philopon <philopon.dependence@gmail.com>
2014-06-09 22:24:32 +0900

    change internal procedure.

--- main.hs
+++ main.hs
@@ -23,27 +23,31 @@ import Data.Monoid
 import Data.Maybe
 import System.Environment
 import System.Process
-import Data.Word
 
-withConfig :: Bool -> Config -> (forall b. Backend b => b -> IO a) -> IO a
-withConfig _     (SQLite a)         f = withConnection a f
-withConfig askPw (PSql h p u mbw d) f = (PSqlConnInfo <$> getCi) >>= \ci -> withConnection ci f
-  where
-    getCi = if askPw
-            then getPassword "password: " >>= \w -> return $ PSql.ConnectInfo h p u w d
-            else return $ PSql.ConnectInfo h p u (maybe "" id mbw) d
+readConfig :: Repo -> IO Config
+readConfig repo =
+    Yaml.decodeFile (encodeString $ repoDirectory repo </> "config") >>=
+    maybe (fail "cannot read config file.") return 
+
+withConfig :: Config -> (forall b. Backend b => b -> IO a) -> IO a
+withConfig (SQLite a) f = withConnection a f
+withConfig (PSql   c) f = withConnection c f
 
 withSavedConfig :: Bool -> Repo -> (forall b. Backend b => b -> IO a) -> IO a
-withSavedConfig askpw repo m = readConfig repo >>= \b -> withConfig askpw b m
+withSavedConfig p repo m = do
+    c  <- readConfig repo
+    c' <- if p then setPasswordIO c else return c
+    withConfig c' m
 
 data Config
     = SQLite { sqliteConnInfo :: ConnectInfo SQLite.Connection }
-    | PSql   { psqlHost :: String
-             , psqlPort :: Word16
-             , psqlUser :: String
-             , psqlPass :: Maybe String
-             , psqlDb   :: String
-             }
+    | PSql   { psqlConnInfo   :: ConnectInfo PSql.Connection   }
+
+setPasswordIO :: Config -> IO Config
+setPasswordIO s@SQLite{} = return s
+setPasswordIO (PSql (PSqlConnInfo c)) = do
+    p <- getPassword "password: "
+    return . PSql $ PSqlConnInfo c { PSql.connectPassword = p }
 
 getPassword :: String -> IO String
 getPassword prompt = do
@@ -59,14 +63,14 @@ instance Yaml.FromJSON Config where
         backend <- o Yaml..: "backend"
         case backend :: String of
             "sqlite"     -> SQLite . SQLiteConnInfo <$> o Yaml..: "connect_info"
-            "postgresql" -> pci
+            "postgresql" -> PSql   . PSqlConnInfo   <$> pci
             _            -> fail "unknown backend."
       where
-        pci = PSql
+        pci = PSql.ConnectInfo
             <$> o Yaml..:  "host"
             <*> o Yaml..:  "port"
             <*> o Yaml..:  "user"
-            <*> o Yaml..:? "password"
+            <*> o Yaml..:? "password" Yaml..!= ""
             <*> o Yaml..:  "database"
     parseJSON _ = fail "not object"
 
@@ -75,21 +79,19 @@ instance Yaml.ToJSON Config where
         [ "backend"      Yaml..= ("sqlite" :: T.Text)
         , "connect_info" Yaml..= ci
         ]
-    toJSON (PSql h p u mbw d) = Yaml.object $ maybe id ((:) . ("password" Yaml..=)) mbw $
+    toJSON (PSql (PSqlConnInfo ci)) = Yaml.object $
+        (if Prelude.null $ PSql.connectPassword ci 
+         then id
+         else (("password" Yaml..= PSql.connectPassword ci):))
         [ "backend"  Yaml..= ("postgresql" :: T.Text)
-        , "host"     Yaml..= h
-        , "port"     Yaml..= p
-        , "user"     Yaml..= u
-        , "database" Yaml..= d
+        , "host"     Yaml..= PSql.connectHost ci
+        , "port"     Yaml..= PSql.connectPort ci
+        , "user"     Yaml..= PSql.connectUser ci
+        , "database" Yaml..= PSql.connectDatabase ci
         ]
 
 writeConfig :: Repo -> Config -> IO ()
 writeConfig repo be = Yaml.encodeFile (encodeString $ repoDirectory repo </> "config") be
-
-readConfig :: Repo -> IO Config
-readConfig repo =
-    Yaml.decodeFile (encodeString $ repoDirectory repo </> "config") >>=
-    maybe (fail "cannot read config file.") return 
 
 helper :: Parser (a -> a)
 helper = abortOption ShowHelpText $ mconcat
@@ -98,27 +100,28 @@ helper = abortOption ShowHelpText $ mconcat
   , hidden ]
 
 config :: Maybe String -> Parser Config
-config user = subparser $
-    command "sqlite"     (info (helper <*> sqlite) $ progDesc "sqlite backend") <>
-    command "postgresql" (info (helper <*> psql)   $ progDesc "postgresql backend")
+config user = subparser $ mconcat 
+    [ command "sqlite"     (info (helper <*> sqlite) $ progDesc "sqlite backend")
+    , command "postgresql" (info (helper <*> psql)   $ progDesc "postgresql backend")
+    ]
   where
     sqlite = SQLite . SQLiteConnInfo <$> argument str (metavar "DB" <> help "sqlite database file.")
     showDefStr = showDefaultWith id
-    psql   = PSql
+    psql   = fmap (PSql . PSqlConnInfo) $ PSql.ConnectInfo
         <$> strOption (short 'h' <> long "host" <> value "localhost" <> metavar "HOST" <> help "postgresql host" <> showDefStr)
         <*> option    (short 'p' <> long "port" <> value 5432        <> metavar "PORT" <> help "postgresql port" <> showDefault)
         <*> strOption (short 'u' <> long "user" <> maybe mempty value user <> metavar "USER" <> help "postgresql user" <> showDefStr) 
-        <*> optional (strOption $ short 'w' <> long "password" <> metavar "PWD" <> help "postgresql password")
+        <*> strOption (short 'w' <> long "password" <> value "" <> metavar "PWD" <> help "postgresql password")
         <*> strOption (short 'd' <> long "database" <> maybe mempty value user <> metavar "DB" <> help "postgresql database" <> showDefStr)
 
 data Action
-    = InitRepo   { optConfig :: Config, askPassword :: Bool}
-    | ShowStatus { askPassword :: Bool }
+    = InitRepo      { optConfig :: Config, askPassword :: Bool}
+    | ShowStatus    { askPassword :: Bool }
     | ShowLog
-    | NewScheme{ askPassword :: Bool }
-    | UpScheme{ askPassword :: Bool }
-    | DownScheme{ askPassword :: Bool }
-    | CurrentScheme{ askPassword :: Bool }
+    | NewScheme     { askPassword :: Bool }
+    | UpScheme      { askPassword :: Bool }
+    | DownScheme    { askPassword :: Bool }
+    | CurrentScheme { askPassword :: Bool }
 
 options :: Maybe String -> Parser Action
 options user = subparser $ mconcat
@@ -145,7 +148,7 @@ main = do
     repo = Repo ".schememilk"
 
 doAction :: Repo -> Action -> IO ()
-doAction repo InitRepo{..} = withConfig askPassword optConfig $ \conn -> do
+doAction repo InitRepo{..} = setPasswordIO optConfig >>= \c -> withConfig c $ \conn -> do
     initRepo conn repo
     writeConfig repo optConfig
     writeFile (repoDirectory repo </> "template.yml") "description: \nup: \ndown: \n"
