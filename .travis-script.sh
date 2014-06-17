3b71370c51069855c10bb0fa8e43e17119b095ad
philopon <philopon.dependence@gmail.com>
2014-06-18 01:26:11 +0900

    add travis.

---
+++ .travis-script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash -eu
+
+printHeader () {
+  echo "################################################################################"
+  echo "                                   " $1
+  echo "################################################################################"
+}
+
+printHeader schememilk
+cabal configure --enable-tests
+cabal test
+cabal install
+
+for path in `cat submodules`; do
+  cd $path
+  printHeader $path
+  cabal configure --enable-tests
+  cabal test
+  cabal install
+  cd ..
+done
